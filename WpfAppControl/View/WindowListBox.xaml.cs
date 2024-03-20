87ce3da815042f482b0b02fafbc5e5153704a48a
Your Name <you@example.com>
2024-03-20 20:53:31 +0300

    add WindowListBox

---
+++ WpfAppControl/View/WindowListBox.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppControl.View
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowListBox.xaml
+    /// </summary>
+    public partial class WindowListBox : Window
+    {
+        List<string> listCollection = new List<string>();
+        StreamReader streamCollection = new StreamReader("persons.txt");
+        public WindowListBox()
+        {
+            InitializeComponent();
+            FillCollection(listCollection, streamCollection);
+            listBoxAutofill.ItemsSource = listCollection;   
+        }
+
+        private void FillCollection(List<string> list, StreamReader stream)
+        {
+            while (!stream.EndOfStream) 
+            { 
+                string line = stream.ReadLine();
+                list.Add(line);
+            }
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var item = listBoxPerson.SelectedValue;
+            listBoxPerson.Items.Add(item);
+        }
+
+        private void buttonDel_Click(object sender, RoutedEventArgs e)
+        {
+            var item = listBoxAutofill.SelectedValue.ToString();
+            listCollection.Remove(item);
+            listBoxAutofill.Items.Refresh();
+            
+        }
+    }
+}
